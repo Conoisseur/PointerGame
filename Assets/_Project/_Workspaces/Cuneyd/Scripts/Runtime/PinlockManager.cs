bb4647b1a823202b93b8d51a44f84626ad310576
B1naryB0b <35399675+B1naryB0b@users.noreply.github.com>
2024-10-23 17:00:03 +0100

    Combination lock (Incomplete)

---
+++ Assets/_Project/_Workspaces/Cuneyd/Scripts/Runtime/PinlockManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PinlockManager : MonoBehaviour
+{
+    
+    [SerializeField] private List<int> unlockedPins = new List<int>();
+    
+    private List<int> _pins = new List<int>();
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
