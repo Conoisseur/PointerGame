77ac0dfce0244c173c3e3c4ba5127110f492d5cc
B1naryB0b <35399675+B1naryB0b@users.noreply.github.com>
2024-11-06 22:37:52 +0000

    Display pins

--- Assets/_Project/_Workspaces/Cuneyd/Scripts/Runtime/PinlockManager.cs
+++ Assets/_Project/_Workspaces/Cuneyd/Scripts/Runtime/PinlockManager.cs
@@ -1,23 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class PinlockManager : MonoBehaviour
 {
     
-    [SerializeField] private List<int> unlockedPins = new List<int>();
+    [SerializeField] private List<int> unlockedPins;
+    private List<int> _currentPins;
     
-    private List<int> _pins = new List<int>();
+    [SerializeField] private GameObject pinPrefab;
     
-    // Start is called before the first frame update
+    private TextMeshProUGUI[] _pinTexts;
+    
+
     void Start()
     {
+        for (int i = 0; i < unlockedPins.Count; i++)
+        {
+            Instantiate(pinPrefab, this.transform);
+        }
         
+        _pinTexts = GetComponentsInChildren<TextMeshProUGUI>();
+
+        for (int i = 0; i < _pinTexts.Length; i++)
+        {
+            _pinTexts[i].text = _currentPins[i].ToString();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void UpdatePins()
     {
         
     }
+    
 }
